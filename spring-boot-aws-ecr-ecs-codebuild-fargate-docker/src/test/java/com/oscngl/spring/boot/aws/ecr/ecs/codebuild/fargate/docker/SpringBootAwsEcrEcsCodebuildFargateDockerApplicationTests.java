eeebb06268cd88fef6a28e1949ea6a9f7b902629
oscngl <oscngl@gmail.com>
2022-08-20 19:31:49 +0300

    AWS ECR & ECS & Fargate & Docker with Spring Boot Application

---
+++ spring-boot-aws-ecr-ecs-codebuild-fargate-docker/src/test/java/com/oscngl/spring/boot/aws/ecr/ecs/codebuild/fargate/docker/SpringBootAwsEcrEcsCodebuildFargateDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscngl.spring.boot.aws.ecr.ecs.codebuild.fargate.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsEcrEcsCodebuildFargateDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
